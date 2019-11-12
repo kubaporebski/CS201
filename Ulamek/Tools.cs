6ca6e6db5c4784aa1abc589cf763ac37404b8660
Porębski Jakub <jakub.porebski@microsoft.wsei.edu.pl>
2019-11-12 19:59:08 +0100

    Pierwszy kommit :)

---
+++ Ulamek/Tools.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kpp.cs201
+{
+    /// <summary>
+    /// Klasa z narzędziami.
+    /// Zawiera metody pomocnicze, które nie wchodzą bezpośrednio w kompetencje klasy Ułamek, ale z których owa klasa korzysta.
+    /// </summary>
+    public static class Tools
+    {
+        /// <summary>
+        /// Wyliczenie NWD metodą Euklidesa.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int NWD(int a, int b)
+        {
+            int tmp;
+            while (b != 0)
+            {
+                tmp = a % b;
+                a = b;
+                b = tmp;
+            }
+            return a;
+
+
+        }
+    }
+}
---
+++ UlamekAsClassUnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UlamekAsClassUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
